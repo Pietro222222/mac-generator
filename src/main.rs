204dd2f539312b7cab61ae63da7a25244b0d73a4
buzz <62407732+Pietro222222@users.noreply.github.com>
2021-12-24 17:08:46 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,3 @@
-use rand::RngCore;
-use std::fmt;
-use std::fmt::Display;
-
-#[derive(Debug)]
-struct MacAdress([u8; 6]);
-
-impl Display for MacAdress {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let octet = &self.0;
-        write!(
-            f,
-            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
-            octet[0], octet[1], octet[2], octet[3], octet[4], octet[5],
-        )
-    }
-}
-
-impl MacAdress {
-    fn new() -> MacAdress {
-        let mut octets: [u8; 6] = [0; 6];
-        rand::thread_rng().fill_bytes(&mut octets);
-        octets[0] |= 0b_0000_0011;
-        MacAdress { 0: octets }
-    }
-    fn is_local(&self) -> bool {
-        (self.0[0] & 0b_0000_0010) == 0b_0000_0010
-    }
-    fn is_unicast(&self) -> bool {
-        (self.0[0] & 0b_0000_0001) == 0b_0000_0001
-    }
-}
 fn main() {
-    let mac = MacAdress::new();
-    assert!(mac.is_local());
-    assert!(mac.is_unicast());
-    println!("Mac generated: {}", mac);
+    println!("MAC ADRESS: {}", "fodsae");   
 }
